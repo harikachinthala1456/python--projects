d566e11999a19af56042b9cfb544df26492b5e4f
harikachinthala <9705122272upendraa@gmail.com>
2026-07-26 08:34:36 +0530

    Add function to reverse a given number project
    reverse a given number project

---
+++ reverse_number.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a number: "))
+
+reverse = 0
+
+while number > 0:
+    digit = number % 10
+    reverse = reverse * 10 + digit
+    number = number // 10
+
+print("Reversed number =", reverse)
