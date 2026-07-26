8cb8d6af25644de6a236a5dec99cb5128f2f9b03
harikachinthala <9705122272upendraa@gmail.com>
2026-07-26 18:01:51 +0530

    Add sum of digits calculation in sum_of_digits.py project
    sum of digits calculation project

---
+++ sum_of_digits.py
@@ -0,0 +1,10 @@
+number = int(input("Enter a number: "))
+
+total = 0
+
+while number > 0:
+     digit = number % 10
+     total = total + digit
+     number =  number // 10
+
+print("Sum of digits =", total)
