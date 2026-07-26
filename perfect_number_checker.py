8f3fe28130048a5a7c8857879ce5c9fe5e9ea7c4
harikachinthala <9705122272upendraa@gmail.com>
2026-07-26 17:48:46 +0530

    Add perfect number checker implementation project
    perfect number checker implementation project

---
+++ perfect_number_checker.py
@@ -0,0 +1,13 @@
+number = int(input("Enter a number: "))
+
+total = 0
+
+for i in range(1, number):
+    if number % i == 0:
+       total = total + i
+
+if total == number:
+    print("Perfect Number")
+else:
+     print("Not a Perfect Number")
+
